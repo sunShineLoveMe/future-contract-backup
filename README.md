1770cf733ca1012e7e8656715025bcfd4ba72138
sunShineLoveMe <juneandrao@outlook.com>
2023-07-13 22:17:09 +0800

    优化插入层级结构

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# future-contract-backup
+# 本地上传文件夹下的所有层级的.csv中的数据
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const XLSX = require('node-xlsx')
-const fs = require('fs')
-const path = require('path')
-const csv = require('csv-parser')
-const mysql = require('mysql')
+const fs = require('fs');
+const path = require('path');
+const csv = require('csv-parser');
+const mysql = require('mysql');
 
 // MySQL 配置
 const connection = mysql.createConnection({
@@ -12,91 +11,114 @@ const connection = mysql.createConnection({
     database: 'contract_data'
 });
 
-// 本地项目文件夹路径
-const folderPath = path.resolve(__dirname, './contractData')
+const directoryPath = path.resolve(__dirname, './contractData');
 
-// 目录表名
-const directoryTableName = 'directories';
+// 定义递归函数，用于读取目录下的所有文件和子目录
+function readDirectory(directoryPath, parentId) {
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
 
-// 数据表名
-const dataTableName = 'stocks';
+    // 遍历所有文件和子目录
+    files.forEach(file => {
+      const filePath = path.join(directoryPath, file);
 
-// 创建目录表
-connection.query(`
-  CREATE TABLE IF NOT EXISTS ${directoryTableName} (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-  )
-`, err => {
-    if (err) throw err;
+      // 如果是子目录，则将其作为菜单项添加到 menu 表中
+      if (fs.statSync(filePath).isDirectory()) {
+        const menuName = path.basename(filePath);
+        let menuId = null;
 
-    console.log(`Table '${directoryTableName}' created`);
-});
-
-
-// 创建数据表
-connection.query(`
-  CREATE TABLE IF NOT EXISTS ${dataTableName} (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    directory_id INT(11) NOT NULL,
-    date DATE NOT NULL,
-    open FLOAT NOT NULL,
-    high FLOAT NOT NULL,
-    low FLOAT NOT NULL,
-    close FLOAT NOT NULL,
-    volume INT(11) NOT NULL,
-    money BIGINT(20) NOT NULL,
-    open_interest INT(11) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (directory_id) REFERENCES ${directoryTableName}(id)
-  )
-`, err => {
-    if (err) throw err;
-
-    console.log(`Table '${dataTableName}' created`);
-});
-
-// 递归函数，用于获取制定文件夹中的所有CSV文
-function readCSVs(folderPath, directoryId) {
-    // 获取文件夹中的所有文件夹和文件
-    fs.readdir(folderPath, (err, files) => {
-        if (err) throw err;
-        // 遍历文件夹中的所有文件和文件夹
-        files.forEach(file => {
-            // 获取文件或文件夹的绝对路径
-            const filePath = path.join(folderPath, file)
-            // 判断是否为文件夹
-            if (fs.statSync(filePath).isDirectory()) {
-                // 如果是文件夹，则将该目录信息插入到目录表中，并递归调用 readCSVs 函数
-                connection.query(`INSERT INTO ${directoryTableName} (name) VALUES (?)`, [file], (err, result) => {
-                    if (err) throw err;
-
-                    const directoryId = result.insertId;
-                    readCSVs(filePath, directoryId);
-                });
-            } else if (path.extname(filePath) === '.csv') {
-                // 如果是csv文件,那么读取该文件
-                fs.createReadStream(filePath)
-                    .pipe(csv())
-                    .on('data', row => {
-                        connection.query(`INSERT INTO ${dataTableName} (directory_id, date, open, high, low, close, volume, money, open_interest) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`, [directoryId, row.date, row.open, row.high, row.low, row.close, row.volume, row.money, row.open_interest], (err, result) => {
-                            if (err) throw err;
-                        });
-                    })
-                    .on('end', () => {
-                        console.log(`Finished reading ${filePath}`)
-                    })
+        // 将菜单项添加到 menu 表中，并获取其自动生成的 ID
+        connection.query(
+          'INSERT INTO menu (name, parent_id, status) VALUES (?, ?, ?)',
+          [menuName, parentId, 'active'],
+          (err, result) => {
+            if (err) {
+              console.error(err);
+              return;
             }
-        })
-    })
+
+            menuId = result.insertId;
+
+            // 递归调用 readDirectory 函数，以处理子目录
+            readDirectory(filePath, menuId);
+          }
+        );
+      } else if (path.extname(filePath) === '.csv') {
+        // 如果是 CSV 文件，则将其数据添加到 stock 表中
+        const stockName = path.basename(filePath, '.csv');
+        connection.query(
+          'INSERT INTO menu (name, parent_id, status) VALUES (?, ?, ?)',
+          [stockName, parentId, 'active'],
+          (err, result) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+
+            const stockId = result.insertId;
+
+            fs.createReadStream(filePath)
+              .pipe(csv())
+              .on('data', row => {
+                connection.query(
+                  'INSERT INTO stock (menu_id, date, open, high, low, close, volume, money, open_interest) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                  [stockId, row.date, row.open, row.high, row.low, row.close, row.volume, row.money, row.open_interest],
+                  err => {
+                    if (err) {
+                      console.error(err);
+                      return;
+                    }
+                  }
+                );
+              })
+              .on('end', () => {
+                console.log(`Finished reading ${filePath}`);
+              });
+          }
+        );
+      }
+    });
+  });
 }
 
-// 调用 readCSVs 函数，开始读取 CSV 文件
-connection.query(`INSERT INTO ${directoryTableName} (name) VALUES (?)`, [folderPath], (err, result) => {
-    if (err) throw err;
+// 建立 MySQL 数据库连接
+connection.connect(err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-    const directoryId = result.insertId;
-    readCSVs(folderPath, directoryId);
+  console.log('Connected to MySQL database.');
+
+  // 建立 menu 表
+  connection.query(
+    'CREATE TABLE IF NOT EXISTS menu (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), parent_id INT, status VARCHAR(255), create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (id), FOREIGN KEY (parent_id) REFERENCES menu(id))',
+    err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      console.log('Created menu table.');
+
+      // 建立 stock 表
+      connection.query(
+        'CREATE TABLE IF NOT EXISTS stock (id INT NOT NULL AUTO_INCREMENT, menu_id INT NOT NULL, date DATE NOT NULL, open FLOAT NOT NULL, high FLOAT NOT NULL, low FLOAT NOT NULL, close FLOAT NOT NULL, volume INT NOT NULL, money BIGINT NOT NULL, open_interest INT NOT NULL, PRIMARY KEY (id), FOREIGN KEY (menu_id) REFERENCES menu(id))',
+        err => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          console.log('Created stock table.');
+
+          // 调用 readDirectory 函数，开始读取目录下的所有文件和子目录
+          readDirectory(directoryPath, null);
+        }
+      );
+    }
+  );
 });
-
